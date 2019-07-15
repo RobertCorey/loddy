c225e268ed7e24f727ce365ea333eeba7e1dc4a1
Rob Corey <robertbcorey@gmail.com>
2019-07-15 18:53:48 -0400

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyC0p5bpLQmdVK6WAGpB3nncj10NREvBUN4',
+    authDomain: 'loddy-e37f1.firebaseapp.com',
+    databaseURL: 'https://loddy-e37f1.firebaseio.com',
+    projectId: 'loddy-e37f1',
+    storageBucket: 'loddy-e37f1.appspot.com',
+    messagingSenderId: '647976948351',
+    appId: '1:647976948351:web:919d258126a461e9'
+  }
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
