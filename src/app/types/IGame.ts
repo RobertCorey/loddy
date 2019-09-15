b51c66ecce14a8244d4959ec3575859ca531c6e5
Rob Corey <robertbcorey@gmail.com>
2019-09-15 15:57:11 -0400

    questions/ answers

--- src/app/types/IGame.ts
+++ src/app/types/IGame.ts
@@ -4,4 +4,6 @@ export interface IGame {
   status: 'LOBBY' | 'BRAIN_QUESTIONS' | 'IN_PROGRESS' | 'FINISHED';
   createdAt: number;
   players: IPlayer[];
+  questions?: any[];
+  answers?: any[];
 }
