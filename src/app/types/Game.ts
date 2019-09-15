dc5a2698e4d4889a9cea4ac0c20f550512beb5cb
Rob Corey <robertbcorey@gmail.com>
2019-09-15 15:19:05 -0400

    add game class

---
+++ src/app/types/Game.ts
@@ -0,0 +1,13 @@
+import { IGame } from './IGame';
+export class Game {
+  constructor(private _game: IGame) {}
+
+  get canGameBeStarted() {
+    return this._game.status === 'LOBBY' && this._game.players.length >= 3;
+  }
+
+  isPlayerHost(playerId: string) {
+    const host = this._game.players.find(player => player.host);
+    return host.id === playerId;
+  }
+}
