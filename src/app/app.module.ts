f61cea2c10bc0902fee822657613e971d7d5b4b8
Rob Corey <robertbcorey@gmail.com>
2019-12-29 15:56:10 -0500

    extract lobby to it's own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { AnswerQuestionFormComponent } from './answer-question-form/answer-quest
 import { QuestionWithAnswerInputComponent } from './question-with-answer-input/question-with-answer-input.component';
 import { GameLoopComponent } from './game-loop/game-loop.component';
 import { PlayersYetToAnswerComponent } from './players-yet-to-answer/players-yet-to-answer.component';
+import { LobbyComponent } from './lobby/lobby.component';
 
 @NgModule({
   declarations: [
@@ -38,7 +39,8 @@ import { PlayersYetToAnswerComponent } from './players-yet-to-answer/players-yet
     AnswerQuestionFormComponent,
     QuestionWithAnswerInputComponent,
     GameLoopComponent,
-    PlayersYetToAnswerComponent
+    PlayersYetToAnswerComponent,
+    LobbyComponent
   ],
   imports: [
     BrowserModule,
