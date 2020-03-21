25029008727df4222dba044689cf2eaa4e6106ac
tomcorey26 <tomcorey26@my.uri.edu>
2020-03-21 17:02:17 -0400

    Css styling for game page, responsiveness still needed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { StateMockerService } from './state-mocker.service';
+import { Component, OnInit } from "@angular/core";
+import { StateMockerService } from "./state-mocker.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
   constructor(private stateMockerService: StateMockerService) {}
   ngOnInit() {
     // this.stateMockerService.answerBrainQuestionsInitial();
     // this.stateMockerService.fullLobbyAsHost();
-    // this.stateMockerService.firstQuestion();
-    this.stateMockerService.firstScoreScreen();
+    this.stateMockerService.firstQuestion();
+    // this.stateMockerService.firstScoreScreen();
   }
 }
