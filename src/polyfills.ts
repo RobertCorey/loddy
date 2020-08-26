a3ca34f17c59a6e059353d6dd7815bc2a0c7f19e
Robert Corey <robertbcorey@gmail.com>
2020-08-26 18:59:11 -0400

    random shit

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -60,3 +60,9 @@ import "zone.js/dist/zone"; // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+declare module "@angular/core" {
+  interface ModuleWithProviders<T = any> {
+    ngModule: Type<T>;
+    providers?: Provider[];
+  }
+}
